169c24d0e0c6507cd63e8a87e9cca42b99104406
KevinR117 <59045597+KevinR117@users.noreply.github.com>
2020-01-11 19:09:46 +0100

    Apply coding rules

--- cesarcode.cpp
+++ cesarcode.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-
 static string alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
 class encryption{
@@ -13,25 +12,19 @@ class encryption{
 public:
 
     encryption() : m_shift(0)
-    {
-
-    }
+    {}
 
     encryption(int shift) : m_shift(shift)
     {
-        if (m_shift > 26)
-        {
-            m_shift = m_shift % 26;
-        }
+        if(m_shift > 26)
+        { m_shift = m_shift % 26; }
     }
 
     char operator()(char c)
     {
         string::iterator letter(alphabet.begin());
-        while (*letter != c and letter != alphabet.end())
-        {
-            letter+=1;
-        }
+        while(*letter != c and letter != alphabet.end())
+        { letter+=1; }
         letter += m_shift;
         return *letter;
     }
@@ -53,22 +46,16 @@ int main()
     cin >> shift;
 
     encryption f(shift);
-
     string::iterator it_text;
 
-    for (it_text = text.begin(); it_text!=text.end(); ++it_text)
-    {
-        *it_text = f(*it_text);
-    }
+    for(it_text = text.begin(); it_text!=text.end(); ++it_text)
+    { *it_text = f(*it_text); }
 
     ostream_iterator<char> show(cout, ", ");
-
     string::iterator encrypted_letter;
 
     for(encrypted_letter = text.begin(); encrypted_letter != text.end(); ++encrypted_letter)
-    {
-        *show = *encrypted_letter;
-    }
+    { *show = *encrypted_letter; }
 
     return 0;
 }
